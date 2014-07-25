62e6a56c3d5f22162e38938ad1e3767938e70ed1
GiroG200 <valderramaster@gmail.com>
2014-07-25 16:38:44 -0400

    Prueba

---
+++ SpotifyAPI/SpoitfyLocalAPI/RemoteHandler.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Threading.Tasks;
+using System.Threading;
+using Newtonsoft.Json;
+
+namespace SpotifyAPI.SpotifyLocalAPI
+{
+    class RemoteHandler
+    {
+        private static RemoteHandler instance = new RemoteHandler();
+
+        public String oauthKey {get;private set;}
+        public String cfidKey {get;private set;}
+
+        public String host = "127.0.0.1";
+
+        ExtendedWebClient wc;
+        internal static RemoteHandler GetInstance()
+        {
+            return instance;
+        }
+
+        internal RemoteHandler()
+        {
+
+            wc = new ExtendedWebClient();
+            wc.Timeout = 2000;
+            wc.Proxy = null;
+            wc.Headers.Add("Origin", "https://embed.spotify.com");
+            wc.Headers.Add("Referer", "https://embed.spotify.com/?uri=spotify:track:5Zp4SWOpbuOdnsxLqwgutt");
+        }
+        internal Boolean Init()
+        {
+            oauthKey = GetOAuthKey();
+            cfidKey = GetCFID();
+            if (cfidKey == "")
+                return false;
+            return true;
+        }
+        internal void SendPauseRequest()
+        {
+            query("remote/pause.json?pause=true", true, true, -1);
+        }
+        internal void SendPlayRequest()
+        {
+            query("remote/pause.json?pause=false", true, true, -1);
+        }
+        internal void SendPlayRequest(String url)
+        {
+            query("remote/play.json?uri=" + url, true, true, -1);
+        }
+        internal StatusResponse Update()
+        {
+            String response = query("remote/status.json", true, true, -1);
+            if(response == "")
+            {
+                return null;
+            }
+            response = response.Replace("\\n", "");
+            byte[] bytes = Encoding.Default.GetBytes(response);
+            response = Encoding.UTF8.GetString(bytes);
+            List<StatusResponse> raw = (List<StatusResponse>)JsonConvert.DeserializeObject(response,typeof(List<StatusResponse>));
+            return raw[0];
+        }
+        internal String GetOAuthKey()
+        {
+            String raw = "";
+            using(WebClient wc = new WebClient())
+            {
+                wc.Proxy = null;
+                raw = wc.DownloadString("http://open.spotify.com/token");
+            }
+            Dictionary<String, object> lol = (Dictionary<String, object>)JsonConvert.DeserializeObject<Dictionary<String, object>>(raw);
+            return (String)lol["t"];
+        }
+
+        internal String GetCFID()
+        {
+            string a = query("simplecsrf/token.json", false, false, -1);
+            a = a.Replace(@"\", "");
+            List<CFID> d = (List<CFID>)JsonConvert.DeserializeObject(a, typeof(List<CFID>));
+            if (d == null)
+                return "";
+            if (d.Count != 1)
+                throw new Exception("CFID couldn't be loaded");
+            if (d[0].error != null)
+                return "";
+            return d[0].token;
+        }
+        internal String query(string request, bool oauth, bool cfid, int wait)
+        {
+            string parameters = "?&ref=&cors=&_=" + GetTimestamp();
+            if (request.Contains("?"))
+            {
+                parameters = parameters.Substring(1);
+            }
+
+            if (oauth)
+            {
+                parameters += "&oauth=" + oauthKey;
+            }
+            if (cfid)
+            {
+                parameters += "&csrf=" + cfidKey;
+            }
+
+            if (wait != -1)
+            {
+                parameters += "&returnafter=" + wait;
+                parameters += "&returnon=login%2Clogout%2Cplay%2Cpause%2Cerror%2Cap";
+            }
+
+            string a = "http://" + host + ":4380/" + request + parameters;
+            string response = "";
+            try
+            {
+                //Need to find a better solution
+                if (SpotifyLocalAPIClass.IsSpotifyRunning())
+                    response = "[ " + wc.DownloadString(a) + " ]";
+            }
+            catch (Exception z)
+            {
+                return "";
+            }
+            return response;
+        }
+        internal int GetTimestamp()
+        {
+            return Convert.ToInt32((DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0)).TotalSeconds);
+        }
+    }
+}
---
+++ mvSpotify/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SpotifyAPI.SpotifyLocalAPI;
+using System.Threading;
+using SpotifyEventHandler = SpotifyAPI.SpotifyLocalAPI.SpotifyEventHandler;
+
+
+
+namespace mvSpotify
+{
+    public partial class Form1 : Form
+    {
+        SpotifyLocalAPIClass spotify;
+        SpotifyMusicHandler mh;
+        SpotifyEventHandler eh;
+
+        public Form1()
+        {
+            InitializeComponent();
+            spotify = new SpotifyLocalAPIClass();
+            if (!SpotifyLocalAPIClass.IsSpotifyRunning())
+            {
+                spotify.RunSpotify();
+                Thread.Sleep(5000);
+            }
+
+            if (!SpotifyLocalAPIClass.IsSpotifyWebHelperRunning())
+            {
+                spotify.RunSpotifyWebHelper();
+                Thread.Sleep(4000);
+            }
+
+            if (!spotify.Connect())
+            {
+                Boolean retry = true;
+                while (retry)
+                {
+                    if (MessageBox.Show("SpotifyLocal no se pudo cargar!", "Error", MessageBoxButtons.RetryCancel) == System.Windows.Forms.DialogResult.Retry)
+                    {
+                        if (spotify.Connect())
+                            retry = false;
+                        else
+                            retry = true;
+                    }
+                    else
+                    {
+                        this.Close();
+                        return;
+                    }
+                }
+            }
+            mh = spotify.GetMusicHandler();
+            eh = spotify.GetEventHandler();
+        }
+
+        //load sincrono con el spotify
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            spotify.Update();
+            progressBar1.Maximum = (int)mh.GetCurrentTrack().GetLength() * 100;
+            pictureBox1.Image = await spotify.GetMusicHandler().GetCurrentTrack().GetAlbumArtAsync(AlbumArtSize.SIZE_160);
+            pictureBox2.Image = await spotify.GetMusicHandler().GetCurrentTrack().GetAlbumArtAsync(AlbumArtSize.SIZE_640);
+
+            linkLabel1.Text = mh.GetCurrentTrack().GetTrackName();
+            linkLabel1.LinkClicked += (senderTwo, args) => Process.Start(mh.GetCurrentTrack().GetTrackURI());
+            linkLabel2.Text = mh.GetCurrentTrack().GetArtistName();
+            linkLabel2.LinkClicked += (senderTwo, args) => Process.Start(mh.GetCurrentTrack().GetArtistURI());
+            linkLabel3.Text = mh.GetCurrentTrack().GetAlbumName();
+            linkLabel3.LinkClicked += (senderTwo, args) => Process.Start(mh.GetCurrentTrack().GetAlbumURI());
+
+            label9.Text = mh.IsPlaying().ToString();
+            label11.Text = ((int)(mh.GetVolume() * 100)).ToString();
+            label7.Text = mh.IsAdRunning().ToString();
+
+            eh.OnTrackChange += new SpotifyEventHandler.TrackChangeEventHandler(trackchange);
+            eh.OnTrackTimeChange += new SpotifyEventHandler.TrackTimeChangeEventHandler(timechange);
+            eh.OnPlayStateChange += new SpotifyEventHandler.PlayStateEventHandler(playstatechange);
+            eh.OnVolumeChange += new SpotifyEventHandler.VolumeChangeEventHandler(volumechange);
+            eh.SetSynchronizingObject(this);
+            eh.ListenForEvents(true);
+        }
+
+        //metodo que muestra el nivel del volumen
+        private void volumechange(VolumeChangeEventArgs e)
+        {
+            label11.Text = ((int)(mh.GetVolume() * 100)).ToString();
+        }
+
+        //metodo para mostrar el estado de la cancion (True si es que esta en curso y false si esta detenida)
+        private void playstatechange(PlayStateEventArgs e)
+        {
+            label9.Text = e.playing.ToString();
+        }
+
+        //metodo para reasignar los atributos de la cancion(track, artista, album)
+        private async void trackchange(TrackChangeEventArgs e)
+        {
+            progressBar1.Maximum = (int)mh.GetCurrentTrack().GetLength() * 100;
+            linkLabel1.Text = e.new_track.GetTrackName();
+            linkLabel2.Text = e.new_track.GetArtistName();
+            linkLabel3.Text = e.new_track.GetAlbumName();
+            pictureBox1.Image = await e.new_track.GetAlbumArtAsync(AlbumArtSize.SIZE_160);
+            pictureBox2.Image = await e.new_track.GetAlbumArtAsync(AlbumArtSize.SIZE_640);
+            label7.Text = mh.IsAdRunning().ToString();
+        }
+
+        //metodo para asignar el valor al progressbar segun el tiempo de reproduccion de la cancion
+        private void timechange(TrackTimeChangeEventArgs e)
+        {
+            label4.Text = formatTime(e.track_time) + "/" + formatTime(mh.GetCurrentTrack().GetLength());
+            progressBar1.Value = (int)e.track_time * 100;
+        }
+
+        //metodo para obtener el tiempo de reproduccion de la cancion
+        private String formatTime(double sec)
+        {
+            TimeSpan span = TimeSpan.FromSeconds(sec);
+            String secs = span.Seconds.ToString(), mins = span.Minutes.ToString();
+            if (secs.Length < 2)
+                secs = "0" + secs;
+            return mins + ":" + secs;
+        }
+
+        #region BOTONES
+        
+        //boton play
+        private void button1_Click(object sender, EventArgs e)
+        {
+            mh.Play();
+        }
+
+        //boton pause
+        private void button2_Click(object sender, EventArgs e)
+        {
+            mh.Pause();
+        }
+
+        //boton anterior
+        private void button3_Click(object sender, EventArgs e)
+        {
+            mh.Previous();
+        }
+
+        //boton siguiente
+        private void button4_Click(object sender, EventArgs e)
+        {
+            mh.Skip();
+        }
+
+        //boton buscar 
+        private void button5_Click(object sender, EventArgs e)
+        {
+            mh.PlayURL(textBox1.Text);
+        }
+
+        #endregion
+
+        //Accion para dejar en mute la cancion
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                mh.Mute();
+            }
+            else
+            {
+                mh.UnMute();
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            int i;
+            for (i = 0; i <= 1; i++)
+            {
+                label11.Text = ((int)(mh.GetVolume() * 100) + i).ToString();
+            }
+            
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            label11.Text = ((int)(mh.GetVolume() * 100) - 1).ToString();
+        }
+
+    }
+}
